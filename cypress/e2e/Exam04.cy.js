0e2ab18859be8c51474165912302008d6f460c45
phakjiramhsy <snsdlovely@outlook.co.th>
2023-01-31 18:04:11 +0700

    Pang

--- cypress/e2e/Exam04.cy.js
+++ cypress/e2e/Exam04.cy.js
@@ -4,7 +4,28 @@ describe("Example04", () => {
   });
   it("input UserName", () => {
     cy.randomStringFunction(5).then((randomUserName) => {
-      cy.get("div.span").contains("User").find("input").click(cs);
+      cy.get('div>span[c="Username"]')
+        .next("input[type='text']")
+        .type(randomUserName);
+    });
+  });
+  it("input PassWord", () => {
+    cy.randomStringAndNumber(8).then((randomPassWord) => {
+      cy.get('div>span[c="Password"]')
+        .next("input[type='text']")
+        .type(randomPassWord);
+    });
+  });
+  it("input UserNameAndPassWord", () => {
+    cy.randomStringFunction(5).then((randomUserName) => {
+      cy.get('div>span[c="Username"]')
+        .next("input[type='text']")
+        .type(randomUserName);
+    });
+    cy.randomStringAndNumber(8).then((randomPassWord) => {
+      cy.get('div>span[c="Password"]')
+        .next("input[type='text']")
+        .type(randomPassWord);
     });
   });
 });
